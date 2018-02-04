ca254fefb9cc1873294c89a19b185f2ba86dbe4e
kanaanchibani <33105385+kanaanchibani@users.noreply.github.com>
2018-02-04 20:26:59 +0100

    Project upload

---
+++ README.md
@@ -0,0 +1,4 @@
+.checkout
+=========
+
+A Symfony project created on February 4, 2018, 5:10 pm.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig');
+
+    }
+}
